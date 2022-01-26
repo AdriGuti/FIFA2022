3aec473c6f0f1bc2468db46c45cb5a4d94e1245c
Adrià Gutiérrez Soria1 <a2107630@institutmontilivi.cat>
2022-01-26 19:17:13 +0100

    PrimerCanviProva1

--- FIFA2022/Program.cs
+++ FIFA2022/Program.cs
@@ -17,6 +17,7 @@ namespace FIFA2022
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
